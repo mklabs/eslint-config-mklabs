bb22ab509a17c7ea5618bce095b4221d4b190c19
Mickael Daniel <daniel.mickael@gmail.com>
2018-09-28 01:13:25 +0200

    1st

---
+++ index.js
@@ -0,0 +1,31 @@
+// Thanks to https://medium.com/@netczuk/your-last-eslint-config-9e35bace2f99
+
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
+  env: {
+    es6: true
+  },
+
+  rules: {
+    'prettier/prettier': [
+      // customizing prettier rules (unfortunately not many of them are customizable)
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all'
+      }
+    ],
+    eqeqeq: ['error', 'always'],
+
+    semi: ['error', 'always']
+  }
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+# eslint-config-mklabs
+
+ESLint Shareable Config to suit my own taste, based on [Airbnb Style
+Guide](https://github.com/airbnb/javascript) and
+[prettier](https://github.com/prettier/prettier).
+
+## Installation
+
+    npx install-peerdeps --dev eslint-config-mklabs
+
+Then use it in your `.eslintrc` file with:
+
+```js
+{
+  "extends": "mklabs"
+}
+```
